154782f92e0479b038eda931284082e9d6f77beb
Pramod Suryapeth <pramodsuryapeth8228@gmail.com>
2026-02-12 22:14:20 +0530

    Mongourl

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 dotenv = require("dotenv")
 
 const connectDB = async () => {
-  // await mongoose.connect(process.env.MONGODB_URI);
-  await mongoose.connect("mongodb://localhost:27017/chatapp");
+  await mongoose.connect(process.env.MONGODB_URI);
+  // await mongoose.connect("mongodb://localhost:27017/chatapp");
   console.log("MongoDB connected");
 };
 
