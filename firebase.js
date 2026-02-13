c4c53c99cb2434389529425ff2f2e95b5ada1b4e
Pramod Suryapeth <pramodsuryapeth8228@gmail.com>
2026-02-13 21:51:08 +0530

    folder import

--- firebase.js
+++ firebase.js
@@ -2,9 +2,12 @@ const admin = require("firebase-admin");
 const service = require("./firebase-service-account.json")
 
 
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert(service),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
